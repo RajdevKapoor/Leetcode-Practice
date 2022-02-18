615ff8193567399e63f02c9a7b1e56018ede8a29
Rajdev Kapoor <rajdev1409@gmail.com>
2022-02-18 09:32:26 -0500

    Time: 9 ms (86.94%), Space: 7.2 MB (82.88%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -5,6 +5,7 @@ public:
         vector<int> v(n+1, 0);
         v[0]=1;
         
+        // for each coin traversing the possible amount arrays and using pre calculated sum.
         for(int coin:coins){
             for(int i=coin;i<n+1;i++){
                 v[i]+=v[i-coin];
