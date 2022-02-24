b01e143698dda4dd5f4c8131615e45175a54e75e
Rajdev Kapoor <rajdev1409@gmail.com>
2022-02-24 00:21:26 -0500

    Time: 2 ms (30.30%), Space: 6.3 MB (15.28%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     int climbStairs(int n) {
         
-        int one = 1;
-        int two = 1;
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
         
-        for(int i = n-2; i>=0; i--) // Bottom-up
-        {
-            int temp = one;
-            one = one + two;
-            two = temp;
+        for(int i=2;i<=n;i++){
+            
+            int op1 =  dp[i-1];
+            int op2 =  dp[i-2];
+            
+            dp[i]=op1+op2;
+            
         }
-        return one;
+        
+        return dp[n];
     }
 };
