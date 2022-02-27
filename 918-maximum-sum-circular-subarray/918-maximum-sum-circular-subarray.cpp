cb267f5c65e06834cc05729aaf038c801a15d662
Rajdev Kapoor <rajdev1409@gmail.com>
2022-02-27 01:13:19 -0500

    Time: 129 ms (17.03%), Space: 40 MB (34.57%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
+    int kadanes(vector<int>& nums){
+        int sum = 0;
+        int ans=INT_MIN;
+        for(int x:nums){
+            sum+=x;
+            ans=max(sum,ans);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        
+        return ans; 
+    }
+    
     int maxSubarraySumCircular(vector<int>& nums) {
-        bool positive=false; int sum=0,maxi=nums[0];
-        int cmax1=0,gmax1=nums[0];
+        
+        if(nums.size()==0) return 0;
+        
+        int op1 = kadanes(nums);
+        int sum = 0;
+        
         for(int i=0;i<nums.size();i++){
-            if(nums[i]>0) positive=true;
             sum+=nums[i];
-            maxi=max(maxi,nums[i]); 
-            cmax1+=nums[i]; //kadane's algorithm
-            if(nums[i]>cmax1) cmax1=nums[i];
-            gmax1=max(gmax1,cmax1);
-            nums[i]*=-1; //inverting all array elements 
+            nums[i]*=-1;
         }
-
-        if(!positive) return maxi; //if all elements of the array are -ve, we just return max element.
-      int cmax2=0,gmax2=nums[0];
-        for(auto x: nums){
-            cmax2+=x; //kadane's algo on inverted array
-            if(x>cmax2) cmax2=x;
-            gmax2=max(gmax2,cmax2);
-        }
- //gmax1=kadane's solution of one-interval and gmax2=kadane's solution of two-interval subarray       
-        int remove=-1*(gmax2); //min sum subarray to be removed
-        return max(sum-remove,gmax1);
-  
+        
+        int t = kadanes(nums);
+        
+        if(sum+t==0) return op1;
+        
+        return max(op1,sum+t);
+          
+        
     }
 };
