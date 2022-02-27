02c16252b656f4b84561b92126356102a3ca594d
Rajdev Kapoor <rajdev1409@gmail.com>
2022-02-27 01:03:50 -0500

    Time: 153 ms (51.83%), Space: 67.7 MB (54.64%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum=INT_MIN;
-        int ans=0;
+        int sum = 0;
+        int ans=INT_MIN;
         
-        for(auto x: nums){
-            ans+=x;
-            sum=max(sum,ans);
-            if(ans<0) {
-                ans=0;
+        //Kadanes
+        
+        for(int x:nums){
+            sum+=x;
+            ans=max(sum,ans);
+            if(sum<0){
+                sum=0;
             }
+            
             
         }
         
-        return sum;
+        return ans;
+    
     }
 };
