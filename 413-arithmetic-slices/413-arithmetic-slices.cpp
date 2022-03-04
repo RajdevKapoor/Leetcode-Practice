0536b2310d1d5b9cd7076b1bdc833f62d3e51d2b
Rajdev Kapoor <rajdev1409@gmail.com>
2022-03-03 22:34:22 -0500

    Time: 7 ms (20.60%), Space: 7.4 MB (55.35%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -8,13 +8,11 @@ public:
         
         vector <int> dp (nums.size(),0);
         
-        
         for(int i=2;i<nums.size();i++){
             if(nums[i]-nums[i-1] == nums[i-1]-nums[i-2]){
                 dp[i]=dp[i-1]+1;
             }
         }
-        
         
         int ans=0;
         
@@ -23,8 +21,6 @@ public:
         }
         
         return ans;
-        
-        
         
     }
 };
