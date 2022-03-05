9077e7ef9ae020966e785efca39b7bb19da287f8
Rajdev Kapoor <rajdev1409@gmail.com>
2022-03-05 12:30:35 -0500

    Time: 160 ms (18.05%), Space: 29.5 MB (16.74%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        if(n <= 0) return false; // get rid of corner cases 
-        if(n == 1) return true; // base case
-        int t2 = 0, t3 = 0, t5 = 0; //pointers for 2, 3, 5
-        vector<int> k(n);
-        k[0] = 1;
-        for(int i  = 1; i < n ; i ++)
-        {
-            k[i] = min(k[t2]*2,min(k[t3]*3,k[t5]*5));
-            if(k[i] == k[t2]*2) t2++; 
-            if(k[i] == k[t3]*3) t3++;
-            if(k[i] == k[t5]*5) t5++;
+        
+        set <long long int> s;
+        s.insert(1);
+        
+        auto it=s.begin();
+        
+        int i=1;
+        
+        while(i<n){
+            s.insert((*it)*2);
+            s.insert((*it)*3);
+            s.insert((*it)*5);
+            
+            it++;
+            i++;
         }
-        return k[n-1];
+        it = s.begin();
+        advance(it, n-1);
+        return *it;
+        
+        
     }
 };
