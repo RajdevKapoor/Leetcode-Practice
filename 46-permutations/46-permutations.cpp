6d6a95b3b96e48dfbcc00219cbf369f7db8d37a5
Rajdev Kapoor <rajdev1409@gmail.com>
2022-03-09 20:38:27 -0500

    Time: 4 ms (68.66%), Space: 7.6 MB (79.29%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>res;
-        vector<int>vec;
-        DFS(nums, 0, vec, res);
-        return res;
+        DFS(res, nums, 0);
+        return res;        
     }
     
-    void DFS(vector<int>& nums, int pos, vector<int>& vec, vector<vector<int>>& res){
-        if(pos == nums.size()){
-            res.push_back(vec);
+    void DFS(vector<vector<int>>& res, vector<int>& nums, int pos){
+        if(pos == nums.size() - 1){
+            res.push_back(nums);
             return;
         }
-        for(int i = 0; i <= vec.size(); i++){
-            vec.insert(vec.begin() + i, nums[pos]);
-            DFS(nums, pos + 1, vec, res);
-            vec.erase(vec.begin() + i);
+        for(int i = pos; i < nums.size(); i++){
+            swap(nums[pos], nums[i]);
+            DFS(res, nums, pos + 1);
+            swap(nums[pos], nums[i]);
         }
-        return;
     }
 };
