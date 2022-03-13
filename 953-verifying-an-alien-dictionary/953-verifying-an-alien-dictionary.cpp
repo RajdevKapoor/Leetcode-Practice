17e65a3713696a964120e4a9c2641da038111a79
Rajdev Kapoor <rajdev1409@gmail.com>
2022-03-12 23:41:49 -0500

    Time: 5 ms (64.13%), Space: 9.6 MB (54.04%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+   unordered_map<char, char> mp;
+    bool isAlienSorted(vector<string>& words, string order) {        
+      
+        for(int i = 0; i < size(order); i++) mp[order[i]] = i + 'a';  
+        for(auto& word : words)
+            for(auto& c : word) c = mp[c];
+        return is_sorted(begin(words), end(words));
+    } 
+};
