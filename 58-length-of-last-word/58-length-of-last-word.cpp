94534273e282e3a7d95a105966cc1708523df542
Rajdev Kapoor <rajdev1409@gmail.com>
2022-03-22 00:55:06 -0400

    Time: 0 ms (100.00%), Space: 6.6 MB (51.97%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int i=s.size()-1;
+        
+        
+        while(i>=0 and s[i]==' '){
+            i--;
+        }
+        
+        int j = i;
+        
+        while(j>=0 && s[j]!=' '){
+            j--;
+        }
+        
+        return i-j;
+        
+    }
+};
