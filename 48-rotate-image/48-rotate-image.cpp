867d1fba93d4ba2b3b001d118080d4c81f4ea4d2
Rajdev Kapoor <rajdev1409@gmail.com>
2022-06-02 15:59:26 +0530

    Time: 3 ms (58.98%), Space: 7.2 MB (34.01%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,15 +3,16 @@ public:
     void rotate(vector<vector<int>>& vect) {
         
         for (int i = 0; i < vect.size(); i++)
-    {
-        for (int j = 0; j < vect[i].size(); j++)
         {
-            if(i>=j){
-                swap(vect[i][j],vect[j][i]);
-            }
-        }    
-        
-    }
+            for (int j = 0; j < vect[i].size(); j++)
+            {
+                if(i>=j)
+                {
+                    swap(vect[i][j],vect[j][i]);
+                }
+            }    
+
+        }
         
         for(int i=0;i<vect.size();i++){
             reverse(vect[i].begin(),vect[i].end());
@@ -20,13 +21,13 @@ public:
         
         
         for (int i = 0; i < vect.size(); i++)
-    {
-        for (int j = 0; j < vect[i].size(); j++)
         {
-            cout << vect[i][j] << " ";
-        }    
-        cout << endl;
-    }
+            for (int j = 0; j < vect[i].size(); j++)
+            {
+                cout << vect[i][j] << " ";
+            }    
+            cout << endl;
+        }
         
     }
 };
