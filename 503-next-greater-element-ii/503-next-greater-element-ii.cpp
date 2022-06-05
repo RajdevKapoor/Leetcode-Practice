4657ee0a8fa68c558556de74921d912815c62bcd
Rajdev Kapoor <rajdev1409@gmail.com>
2022-06-05 15:25:39 +0530

    Time: 58 ms (32.18%), Space: 27.6 MB (5.17%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -6,32 +6,25 @@ public:
         stack <int> s;
         vector <int> v;
         
-        for(int i=0;i<n;i++){
-            nums.push_back(nums[i]);
-        }
+        for(int i=0;i<n;i++) nums.push_back(nums[i]);
         
-       
-        for(int i=2*n-1;i>-1;i--){
+        
+        for(int i=2*n-1;i>=0;i--){
             
-            while(s.size() > 0 and s.top()<=nums[i]){
+            while(s.size()>0 and s.top()<=nums[i]){
                 s.pop();
             }
             
-            s.empty() ? v.push_back(-1) : v.push_back(s.top());
+            s.empty() ? v.push_back(-1):v.push_back(s.top());
             
             s.push(nums[i]);
             
         }
         
-        reverse(v.begin(), v.end());
-        vector<int> sub_vec;
+        reverse(v.begin(),v.end());
+        vector <int> ans (v.begin(), v.begin()+n);
         
-        for(int i=0;i<n;i++){
-            sub_vec.push_back(v[i]);
-        }
-        
-        v.clear();
-        return sub_vec;
+        return ans;
          
     }
 };
