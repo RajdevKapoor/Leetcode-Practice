44df2f871c5047fa817198d064a77bf211424db9
Rajdev Kapoor <rajdev1409@gmail.com>
2022-06-16 13:40:57 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (13.61%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.cpp
+++ 925-long-pressed-name/925-long-pressed-name.cpp
@@ -4,8 +4,8 @@ public:
         int n = name.size(),m = typed.size(),i = 1, j = 1, k = 0;
         if(name[0] != typed[0] || n > m) return false;
         for(;j<m;j++)
-            if(i != n && name[i] == typed[j]) k = i++;
-            else if(name[k] != typed[j]) return false;
+            if(i != n && name[i] == typed[j]) i++;
+            else if(name[i-1] != typed[j]) return false;
         return i == n && j == m;
     }
 };
