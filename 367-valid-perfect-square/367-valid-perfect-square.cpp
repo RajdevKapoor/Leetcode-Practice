28c4edbc2677b74cd91a13e9bd315948119db855
Rajdev Kapoor <rajdev1409@gmail.com>
2022-06-23 17:03:24 +0530

    Time: 4 ms (24.18%), Space: 5.9 MB (17.11%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
-    bool isPerfectSquare(int nums) {
+    bool isPerfectSquare(int num) {
         
-        int ans = sqrt(nums);
+        if (num == 1) return true;
         
-        return ans*ans==nums;
+        long long nums = num;
         
+        long long l = 1, r = num;
+        
+        while (l <= r){
+            long long mid = l + (r - l) / 2;
+            
+            if (mid * mid == nums) {
+                return true;
+            }
+            
+            else if (mid * mid > nums){
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return false;
     }
 };
