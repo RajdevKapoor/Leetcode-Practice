a1aced6efce143dc6215fc8c39dbb4ba35418879
Rajdev Kapoor <rajdev1409@gmail.com>
2022-06-24 19:21:48 +0530

    Time: 72 ms (8.91%), Space: 28.2 MB (5.80%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -10,18 +10,22 @@ public:
         
         int ans = INT_MAX;
         
-       for(int j=0;j<n;j++){
-           sum+=nums[j];
-           while(sum>=target){
-               ans=min(ans,j-i+1);
-               sum-=nums[i];
-               i++;
-           }
-       }
+        while(j<n){
+            
+            while(j<n and sum<target){
+                sum+=nums[j++];
+            }
+            
+            while(i<j and sum>=target){
+                ans = min(ans,j-i+1);
+                sum-=nums[i++];
+            }
+            
+        }
         
         if(ans==INT_MAX){
             return 0;
         }
-        return ans;
+        return ans-1;
     }
 };
