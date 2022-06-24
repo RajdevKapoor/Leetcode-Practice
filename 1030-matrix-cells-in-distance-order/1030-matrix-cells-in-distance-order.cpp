4d3cbe67a4788f60279fd0927c4dba602beafe94
Rajdev Kapoor <rajdev1409@gmail.com>
2022-06-24 16:16:46 +0530

    Time: 951 ms (5.23%), Space: 87.3 MB (7.60%) - LeetHub

---
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    static bool comp(vector<int>a,vector<int>b)
+    {
+        return a[2]<b[2];
+    }
+    vector<vector<int>> allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        vector<vector<int>>ans;
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                ans.push_back({i,j,abs(i-rCenter)+abs(j-cCenter)});
+            }
+        }
+        sort(ans.begin(),ans.end(),comp);
+        for(vector<int>&i:ans)
+        {
+            i.erase(i.begin()+2);
+        }
+        return ans;
+
+    }
+};
