b4796c1ceb6107daa1b1bbb43425520883ce2c53
Rajdev Kapoor <rajdev1409@gmail.com>
2022-06-25 05:15:29 +0530

    Time: 8 ms (11.28%), Space: 7.3 MB (49.72%) - LeetHub

---
+++ 355-design-twitter/355-design-twitter.cpp
@@ -0,0 +1,27 @@
+class Twitter {
+    vector<pair<int, int>> whoPosted;
+    unordered_map<int, unordered_set<int>> whoFollowWho;
+public:
+    Twitter() {
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        whoPosted.push_back({ userId, tweetId });
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        unordered_set<int> s = whoFollowWho[userId];
+        s.insert(userId);
+        vector<int> ans;
+        for (int i = whoPosted.size() - 1; i > -1 && ans.size() < 10; --i) if (s.find(whoPosted[i].first) != s.end()) ans.push_back(whoPosted[i].second);
+        return ans;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        whoFollowWho[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        whoFollowWho[followerId].erase(followeeId);
+    }
+};
