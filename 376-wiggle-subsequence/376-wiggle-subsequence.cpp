2d072d67519ba34161a61425de1841a4cff0862d
Rajdev Kapoor <rajdev1409@gmail.com>
2022-06-26 17:41:29 +0530

    Time: 3 ms (67.97%), Space: 7.1 MB (42.24%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -5,22 +5,15 @@ public:
         if (nums.size() < 2)
             return nums.size();
         
-        vector<int> up (nums.size()+1,0);
-        vector<int> down (nums.size()+1,0);
-        up[0] = down[0] = 1;
+        int up=1,down=1;
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i] > nums[i - 1]) {
-                up[i] = down[i - 1] + 1;
-                down[i] = down[i - 1];
+                up=down+1;
             } else if (nums[i] < nums[i - 1]) {
-                down[i] = up[i - 1] + 1;
-                up[i] = up[i - 1];
-            } else {
-                down[i] = down[i - 1];
-                up[i] = up[i - 1];
+                down=up+1;
             }
         }
-        return max(down[nums.size() - 1], up[nums.size() - 1]);
+        return max(up,down);
         
     }
 };
