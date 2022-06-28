1960401631e3a7a20eaa595c76a13f302d9b9011
Rajdev Kapoor <rajdev1409@gmail.com>
2022-06-28 22:48:48 +0530

    Time: 15 ms (84.66%), Space: 21.4 MB (40.75%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -13,15 +13,9 @@ class Solution {
 public:
     TreeNode* solve(vector<int>& nums,int s, int e){
         
-        if(s>e){
-            return NULL;
-        }
-        
+        if(s>e) return NULL;
         int mid = (s+e)/2;
-        TreeNode* node = new TreeNode(nums[mid]);
-        node-> left = solve(nums,s,mid-1);
-        node-> right = solve(nums,mid+1,e);
-        return node;
+        return new TreeNode(nums[mid],solve(nums,s,mid-1),solve(nums,mid+1,e));
         
     }
     
