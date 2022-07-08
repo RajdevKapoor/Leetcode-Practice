c1b5d7dc0b8e261cc3e4ab1af8df74ad3801cccd
Rajdev Kapoor <rajdev1409@gmail.com>
2022-07-08 12:46:32 +0530

    Time: 414 ms (22.04%), Space: 56.1 MB (34.83%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -6,7 +6,7 @@ public:
         int ans;
         while(l<=r){
             int m = (l+r)/2;
-            if(numOp(nums, m) <= maxOperations){
+            if(numOp(nums, m,maxOperations)){
                 ans=m;
                 r = m-1;
             } 
@@ -15,14 +15,15 @@ public:
         return ans;
     }
 private:
-    int numOp(vector<int>& nums, int m){
-        int ans =0;
+    bool numOp(vector<int>& nums, int m,int maxOperations){
+        int ans = 0;
         for(int i{}; i<nums.size(); i++){
             if(nums[i] >= m){
                 int s = (nums[i]%m == 0)?(nums[i]/m):(nums[i]/m + 1);
                 ans += s-1;
+                if(ans>maxOperations) return false;
             }
         }
-        return ans;
+        return ans<=maxOperations;
     }
 };
