09dfd9e38d94d08d0223af8cd0b299af14ea2253
Rajdev Kapoor <rajdev1409@gmail.com>
2022-07-10 16:55:59 +0530

    Time: 460 ms (27.00%), Space: 75.3 MB (29.95%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -4,19 +4,19 @@ public:
         
         if(target==0) return true;
         
-        if(i==0) return target==nums[0];
+        if(i==nums.size()) return target==0;
         
         
         if(dp[i][target]!=-1){
             return dp[i][target];
         }
         
-        bool not_take = solve(nums,target,i-1,dp);
+        bool not_take = solve(nums,target,i+1,dp);
         
         bool take = false;
         
         if(nums[i]<=target){
-            take = solve(nums,target-nums[i],i-1,dp);
+            take = solve(nums,target-nums[i],i+1,dp);
         }
         
         return dp[i][target] = not_take or take;
@@ -33,7 +33,7 @@ public:
         
         if(sum&1) return false;
         vector<vector<int>> dp(nums.size(), vector<int>(sum/2+1,-1));
-        return solve(nums,sum/2,nums.size()-1,dp);
+        return solve(nums,sum/2,0,dp);
         
     }
 };
