b25a6858de823f0f54cdcf95538c70b06f26cf3b
Rajdev Kapoor <rajdev1409@gmail.com>
2022-07-23 18:37:12 +0530

    Time: 4 ms (94.66%), Space: 14.9 MB (42.25%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,52 +1,47 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* l3 = NULL;
-       
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* list3 = NULL; 
         
-        if(l1->val > l2->val){
-            l3 = l2;
-            l2 = l2->next;
-        }else{
-            l3 = l1;
-            l1 = l1->next;
+        if(list1 == NULL && list2 == NULL){
+            return NULL;
+        }
+        if(list1 == NULL){
+           return list2; 
+        }else if(list2 == NULL){
+            return list1;
         }
         
-        ListNode* temp = l3;
+        if(list1->val > list2->val){
+            list3 = list2;
+            list2 = list2->next;
+        }else{
+            list3 = list1;
+            list1 = list1->next;
+        }
         
-        while(l1 and l2){
+        ListNode* temp = list3;
+        
+        while(list1 != NULL && list2 != NULL){
+           
             
-            if(l1->val > l2->val){
-                temp->next = l2;
-                l2 = l2->next;
+            if(list1->val < list2->val){
+                temp->next = list1;
+                list1=list1->next;
             }else{
-                temp->next = l1;
-                l1 = l1->next;
+                temp->next = list2;
+                list2 = list2->next;
             }
-            
-            temp=temp->next;
-            
+        
+            temp = temp->next;
         }
         
-        if(l1!=NULL){
-            temp->next = l1;
+        if(list1!=NULL){
+            temp->next = list1;
         }else{
-            temp->next = l2;
+            temp->next = list2;
         }
         
-        return l3;
-        
+        return list3;
     }
 };
