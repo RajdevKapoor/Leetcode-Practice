697ce074b2385c8c87b1828efa65550171aeaf7d
Rajdev Kapoor <rajdev1409@gmail.com>
2022-07-23 17:46:00 +0530

    Time: 195 ms (56.96%), Space: 53.4 MB (73.86%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,5 +1,5 @@
 class Solution {
-private:
+    private:
     bool isPalindrome(string s, int start, int end) {
         while (start <= end) {
           if (s[start++] != s[end--])
@@ -8,23 +8,33 @@ private:
         return true;
       }
 public:
-     void func(int index, string s, vector <string> & path, vector<vector<string>> & res) {
-        if (index == s.size()) {
-          res.push_back(path);
-          return;
+    
+    void solve(int partition, string &s,vector<string> &temp, vector<vector<string>> &ans){
+        
+        if(partition==s.size()){
+            ans.push_back(temp);
+            return;
         }
-        for (int i = index; i < s.size(); i++) {
-              if (isPalindrome(s, index, i)) {
-                    path.push_back(s.substr(index, i - index + 1));
-                    func(i + 1, s, path, res);
-                    path.pop_back();
-              }
+        
+        for(int i=partition;i<s.size();i++){
+            
+            if(isPalindrome(s,partition,i)){
+                temp.push_back(s.substr(partition,i-partition+1));
+                solve(i+1,s,temp,ans);
+                temp.pop_back();
+            }
+            
         }
-      }
+        
+        
+    }
+    
     vector<vector<string>> partition(string s) {
-        vector < vector < string > > res;
-        vector < string > path;
-        func(0, s, path, res);
-        return res;
+        
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(0,s,temp,ans);
+        return ans;
+        
     }
 };
