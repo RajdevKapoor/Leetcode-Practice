b7ac2b1dda3a3267babbfb16e4fe99012c94d809
Rajdev Kapoor <rajdev1409@gmail.com>
2022-07-23 18:44:38 +0530

    Time: 16 ms (30.14%), Space: 15 MB (13.96%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* list3 = NULL; 
         
         if(list1 == NULL && list2 == NULL){
-            return NULL;
+            return list1;
         }
         if(list1 == NULL){
            return list2; 
         }else if(list2 == NULL){
             return list1;
         }
+        ListNode* listResHead =  new ListNode(min(list1->val,list2->val));
+        ListNode* listResCurr = listResHead; 
         
-        if(list1->val > list2->val){
-            list3 = list2;
-            list2 = list2->next;
+        if(list1->val < list2->val){
+            list1=list1->next;
         }else{
-            list3 = list1;
-            list1 = list1->next;
+            list2 = list2->next;
         }
-        
-        ListNode* temp = list3;
         
         while(list1 != NULL && list2 != NULL){
            
+            ListNode* temp = new ListNode();
             
             if(list1->val < list2->val){
-                temp->next = list1;
-                list1=list1->next;
+                temp->val = list1->val;
+                list1 = list1->next;
             }else{
-                temp->next = list2;
+                temp->val = list2->val;
                 list2 = list2->next;
             }
         
-            temp = temp->next;
+            listResCurr->next = temp;
+            listResCurr = listResCurr->next;
+            
         }
         
         if(list1!=NULL){
-            temp->next = list1;
+            listResCurr->next = list1;
         }else{
-            temp->next = list2;
+            listResCurr->next = list2;
         }
-        
-        return list3;
+        return listResHead;
     }
 };
