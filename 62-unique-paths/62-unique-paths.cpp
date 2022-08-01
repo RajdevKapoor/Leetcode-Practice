33d5623fefad86d4c1635f064d77e9ef454dcd81
Rajdev Kapoor <rajdev1409@gmail.com>
2022-08-01 11:14:29 +0530

    Time: 2 ms (50.13%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -11,8 +11,13 @@ public:
         return dp[i][j]=r+d;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
         
-        return solve(dp,m,n,0,0);
+        return dp[0][0];
     }
 };
