ac488210d7c7d4a6e0756d5b2e68eb58805d555f
Rajdev Kapoor <rajdev1409@gmail.com>
2022-08-03 12:46:55 +0530

    Time: 678 ms (11.99%), Space: 82.2 MB (31.86%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canCross(vector<int>& stones) {
+        int n=stones.size();
+        map<int,set<int>>mp;
+        set<int>st;
+        for(auto it : stones)
+            mp[it]=st;
+        mp[stones[0]].insert(1);
+        for(int i=0;i<n;i++){
+            int currStone=stones[i];
+            set<int>jumps=mp[currStone];
+            for(auto jump : jumps){
+                int pos=currStone+jump;
+                if(pos==stones[n-1])
+                    return true;
+                if(mp.find(pos)!=mp.end())
+                {
+                    if(jump-1>0){
+                        mp[pos].insert(jump-1);
+                    }
+                    mp[pos].insert(jump);
+                    mp[pos].insert(jump+1);
+                }
+            }
+        }
+        return false;
+    }
+};
