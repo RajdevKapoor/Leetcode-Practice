b2c36fc150753c4877567a03f39a4bec74d2fb66
Rajdev Kapoor <rajdev1409@gmail.com>
2022-08-03 12:02:32 +0530

    Time: 179 ms (44.07%), Space: 37.7 MB (77.01%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -11,14 +11,13 @@ public:
             v.push_back({start, end-1});
             return true;
         }
-        for (pair<unsigned int, unsigned int> p1:v){
+        for (auto p1:v){
             bool cond1 = (p1.first<=start && p1.second>=start);
             bool cond2 = (p1.first<=end-1 && p1.second>=end-1);
             bool cond3 = (start<=p1.first && end-1>=p1.first);
             bool cond4 = (p1.second>=start && p1.second<=end-1);
             if (cond1 || cond2 || cond3 || cond4)
                 return false;
-
         }
         v.push_back({start, end-1});
         return true;
