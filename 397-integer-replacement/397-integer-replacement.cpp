41c58c1beaae406588a99d7dc75c8ceb561905fa
Rajdev Kapoor <rajdev1409@gmail.com>
2022-08-04 16:51:22 +0530

    Time: 3 ms (67.66%), Space: 5.9 MB (82.43%) - LeetHub

--- 397-integer-replacement/397-integer-replacement.cpp
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -9,12 +9,12 @@ public:
         if(n%2==0){
             return 1+solve(n/2);
         }else{
-            return 1+min(solve(n-1),solve(n+1));
+            return 2+min(solve((n-1)/2),solve((n+1)/2));
         }
         
     }
     
     int integerReplacement(int n) {
-        return solve(n);
+       return solve(n);
     }
 };
