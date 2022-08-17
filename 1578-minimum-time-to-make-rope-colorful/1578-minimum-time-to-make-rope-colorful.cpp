181cce3115a849992239cad97c58062ab40e46da
Rajdev Kapoor <rajdev1409@gmail.com>
2022-08-17 15:55:36 +0530

    Time: 268 ms (40.37%), Space: 95.5 MB (51.61%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -8,11 +8,13 @@ public:
 
         for(int i=0;i<n-1;i++){
             if(s[i]==s[i+1]){
-                if (c[i] > c[i + 1]) swap(c[i], c[i + 1]);
-                cost += c[i];
+                if(c[i]>c[i+1]){
+                   swap(c[i],c[i+1]);
+                }
+                cost+=c[i];
             }
         }
-        
+
         return cost;
         
     }
