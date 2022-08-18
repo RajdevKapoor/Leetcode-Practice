2d951d6c697b6f1e07a26c59a1b6c3beae049a3e
Rajdev Kapoor <rajdev1409@gmail.com>
2022-08-18 18:32:27 +0530

    Time: 5 ms (19.58%), Space: 6.9 MB (9.41%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-   vector<string> letterCombinations(string digits) {
-	if(digits == "") return {};
-	vector<string> mappings{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans{""};
-	for(auto digit : digits){
-		vector<string> extendCombination;
-		for(auto& currentCombination : ans)
-			for(auto newChar : mappings[digit - '2'])
-				extendCombination.push_back(currentCombination + newChar);                            
-		ans = move(extendCombination); // same as ans = extendCombination, just avoids copying each value. You Can also use swap(ans,extendCombination)
-	}
-	return ans;
-}
+    vector<string> res;
+    vector<string> arr = {"","","abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
+    void func(string &digits, int index, string temp, vector<string> arr) {
+        if(index == digits.size()) {
+            
+            res.push_back(temp);
+            return;
+        }
+        
+        for(auto j : arr[digits[index]-'0']){
+            func(digits, index + 1, temp + j, arr);
+        }
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        if(digits.size() == 0) return res;
+        func(digits, 0, "", arr);
+        return res;
+    }
 };
