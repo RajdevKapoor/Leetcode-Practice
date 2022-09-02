9a52ddabfebdab376794701e0a3ae39fa9dd75b3
Rajdev Kapoor <rajdev1409@gmail.com>
2022-09-02 15:22:50 -0400

    Time: 415 ms (10.59%), Space: 200.3 MB (5.02%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     
-    int getdifficulty(int pos , vector<int> &jobs , int d , vector<vector<int>> &dp){
-       int n = jobs.size();
-        
-       if(d == 1){
-           int mx = INT_MIN;
-           for(int i=pos ; i<n ; ++i) mx = max(mx,jobs[i]);
-           return mx;
-       } 
-        
-       if(dp[pos][d] != -1) return dp[pos][d]; 
-        
-       int ans = INT_MAX;
-       int mx = jobs[pos];
-       for(int i=pos+1 ; i<=n-d+1 ; ++i){
-           ans = min(ans,mx + getdifficulty(i,jobs,d-1,dp));
-           
-           mx = max(mx,jobs[i]);
-       } 
-       return dp[pos][d] = ans; 
+
+    int solve(int i,int day, vector<int>&jobDifficulty, vector<vector<int>>&dp)
+    {
+        if(day == 0)
+        {
+            int temp = INT_MIN;
+            for(int p=i;p<jobDifficulty.size();p++)
+            {
+                temp = max(temp, jobDifficulty[p]);
+            }
+            return temp;
+        }
+        if(dp[i][day] != -1)
+        {
+            return dp[i][day];
+        }
+        int temp = INT_MIN;
+        int ans = INT_MAX;
+        for(int k=i; k<=jobDifficulty.size() - day - 1;k++)
+        {
+            
+            temp = max(temp, jobDifficulty[k]);
+            
+            ans = min(ans, temp + solve(k+1, day-1, jobDifficulty, dp));
+        }
+        return dp[i][day] = ans;
     }
 
-    int minDifficulty(vector<int>& jobs, int d) {
-        int n = jobs.size();
-        if(d > n) return -1;
-        vector<vector<int>> dp(n+1,vector<int>(d+1,-1));
-        return getdifficulty(0,jobs,d,dp);
-    } 
+
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        vector<vector<int>>dp(1000, vector<int>(1000,-1));
+        if(d > jobDifficulty.size())
+        {
+            return -1;
+        }
+        return solve(0, d-1, jobDifficulty, dp);
+    }
 };
