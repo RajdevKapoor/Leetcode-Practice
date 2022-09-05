88382786ce67e4c375fc882814ab56561ecae484
Rajdev Kapoor <rajdev1409@gmail.com>
2022-09-05 00:20:42 -0400

    Time: 66 ms (51.94%), Space: 28.2 MB (31.10%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
-        int n = time.size();
+        unordered_map<int, int> map;
         
-        unordered_map<int, int>mp;
-        
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            ans += mp[(60-(time[i]%60))%60];
-            mp[(time[i]%60)]++;
-        }        
-        return ans;        
+        for(int i=0; i<time.size(); i++) {
+            time[i] %= 60;
+        }
+        int count = 0;
+        signed long int zeroCount = 0;
+        for(int i=time.size()-1; i>=0; i--) {
+            int key = 60 - time[i];
+            if(time[i] != 0) {
+                if(map.find(key) != map.end()) {
+                    count += map[key];
+                } 
+                map[time[i]]++;
+            }
+            else {
+                zeroCount+=1;
+            }
+                
+        }
+        if(zeroCount != 0) {
+            zeroCount = zeroCount*(zeroCount-1)/2;
+            return count + zeroCount;
+        }
+        return count;
+    
     }
+    
 };
