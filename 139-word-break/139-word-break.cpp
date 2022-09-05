a6c7f197e1b2ce5e390b7abc1fdde052faae85b7
Rajdev Kapoor <rajdev1409@gmail.com>
2022-09-05 16:15:04 -0400

    Time: 41 ms (23.13%), Space: 22.2 MB (7.86%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int dp[301];
-    int help(int i,string s, set<string> wordDist){
+    int help(int i,string &s, set<string> wordDist){
+        
         if(i == s.size()) return 1;
+        
         string temp;
+        
         if(dp[i]!=-1)return dp[i];
+        
         for(int j = i; j<s.size();j++){
             temp+=s[j];
             if(wordDist.find(temp)!=wordDist.end())
-                if(help(j+1,s,wordDist))return dp[i] = 1;
+                if(help(j+1,s,wordDist)) return dp[i] = 1;
             
         }
+        
         return dp[i] = 0;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
