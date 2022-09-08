caf4eff7ef825b0fae870c054b3bd83ab56564e9
Rajdev Kapoor <rajdev1409@gmail.com>
2022-09-08 02:45:34 -0400

    Time: 237 ms (11.69%), Space: 67.8 MB (10.96%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,16 +4,12 @@ public:
         int sum = 0;
         int ans=INT_MIN;
         
-        //Kadanes
-        
         for(int x:nums){
             sum+=x;
             ans=max(sum,ans);
             if(sum<0){
                 sum=0;
             }
-            
-            
         }
         
         return ans;
