27224a3f99022eca7a4d177c69f1382bff08bb0e
Rajdev Kapoor <rajdev1409@gmail.com>
2023-10-10 14:26:31 -0400

    Time: 24 ms (14.99%), Space: 16.7 MB (5.49%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,44 +1,56 @@
 class Solution {
-public: 
-    bool checkCyc(int i, unordered_map <int,vector<int>> &graph,vector<bool> &visited,vector<bool> &currVisited,list<int> &ans){
-        visited[i]=true;
-        currVisited[i]=true;
+public:
+    
+    bool solve(int i, vector<vector<int>>& prerequisites, list<int> &ans, 
+               vector<bool> &visited,vector<bool> &currVisited,unordered_map<int,vector<int>> &g){
+        visited[i] = true;
+        currVisited[i] = true;
         
-        for(auto nbr:graph[i]){
+        
+        for(auto nbr: g[i]){
             if(visited[nbr]==false){
-                if(checkCyc(nbr,graph,visited,currVisited,ans)){
+                if(solve(nbr,prerequisites,ans,visited,currVisited,g)){
                     return true;
                 }
             }else{
-                if(currVisited[nbr]){
+                if(currVisited[nbr]==true){
                     return true;
                 }
             }
+            
         }
+        
         ans.push_front(i);
-        currVisited[i]=false;
+        currVisited[i] = false;
         return false;
     }
     
-    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        unordered_map <int,vector<int>> graph;
-        vector<bool>visited(n, false),currVisited(n,false);
-        for(auto x: prerequisites){
-            int a = x[0];
-            int b = x[1];
-            graph[b].push_back(a);
+    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        unordered_map<int,vector<int>> g;
+        vector<bool>visited(numCourses,0), currVisited(numCourses,0);
+        
+        for(auto p:prerequisites){
+            int x = p[0];
+            int y = p[1];
+            g[y].push_back(x); 
         }
         
         list<int> ans;
-        for(int i=0;i<n;i++){
+        
+        for(int i=0;i<numCourses;i++){
             if(visited[i]==false){
-                if(checkCyc(i,graph,visited,currVisited,ans)){
+                if(solve(i,prerequisites,ans,visited,currVisited,g)){
                     return {};
                 }
+                
             }
         }
-        vector<int> result(ans.begin(),ans.end());
         
+        vector<int> result(ans.begin(),ans.end());
         return result;
+        
+        
     }
 };
