bb341269c86f7b5d03c0adb14c4443b376d558a8
Rajdev Kapoor <rajdev1409@gmail.com>
2023-10-12 23:30:07 -0400

    Time: 9 ms (19.87%), Space: 14 MB (26.23%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -14,9 +14,20 @@ public:
     
     int minCostClimbingStairs(vector<int>& cost) {
         
+        
+        
+        
         vector<int> dp(cost.size(),-1);
-        solve(0,cost,dp);
-        return min(dp[0],dp[1]);
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i=2;i<cost.size();i++){
+            dp[i] = min(cost[i]+dp[i-1],cost[i]+dp[i-2]);
+        }
+        
+        
+        return min(dp[cost.size()-1],dp[cost.size()-2]);
         
         
     }
