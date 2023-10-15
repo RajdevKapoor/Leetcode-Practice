e1ee36e4b7b1708826430f4bb76f46659dd9e1ed
Rajdev Kapoor <rajdev1409@gmail.com>
2023-10-14 22:47:32 -0400

    Time: 34 ms (22.29%), Space: 53.8 MB (10.60%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    int dp[501][501];
-    int M=1e9+7;
-    int solve(int pos, int n, int steps){
-        if(steps==0){
-            if(pos==0)
-                return 1;
+    int mod =1e9 + 7;
+    int solve(int i, int n, int steps, int k,vector<vector<int>>  &mp){
+        
+        if(i<0 or i>=n or k>steps){
             return 0;
         }
-        if(dp[pos][steps]!=-1)
-            return dp[pos][steps];
-        int ans=0;
-        if(pos-1>=0)
-            ans=(ans%M+solve(pos-1,n,steps-1)%M)%M;
-        if(pos+1<n)
-            ans=(ans%M+solve(pos+1,n,steps-1)%M)%M;
-        ans=(ans%M+solve(pos,n,steps-1)%M)%M;
-        return dp[pos][steps]=ans;
+        
+        if(mp[i][k]!=-1){
+            return mp[i][k];
+        }
+        
+        if(k==steps){
+            if(i==0) {
+                return 1;
+            }
+            return 0;
+        }
+        int op1 =solve(i+1,n,steps,k+1,mp)%mod;
+        int op2 =solve(i-1,n,steps,k+1,mp)%mod;
+        int op3 =solve(i,n,steps,k+1,mp)%mod;
+        
+        mp[i][k]=((op1 + op2)%mod +op3)%mod;
+        return mp[i][k];
+        
     }
+    
     int numWays(int steps, int n) {
-        memset(dp,-1,sizeof(dp));
-        if(steps<=n)
-            n=steps;
-        return solve(0,n,steps);
+        vector<vector<int>> mp(501,vector<int>(501,-1));
+        return solve(0,n,steps,0,mp);
     }
 };
