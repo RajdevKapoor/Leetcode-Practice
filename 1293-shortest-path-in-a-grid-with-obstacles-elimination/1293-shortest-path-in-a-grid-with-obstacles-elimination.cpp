4eb37938887a7c57de734b5cb813c057a0da5f02
Rajdev Kapoor <rajdev1409@gmail.com>
2023-10-29 03:55:52 -0400

    Time: 240 ms (22.72%), Space: 68.9 MB (14.66%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    int m, n;
+    vector<pair<int, int>> dirc = {{+1, 0}, {-1, 0}, {0, +1}, {0, -1}};
+    bool isValid(int i, int j){
+        if(i>=0 && i<m && j>=0 && j<n) return true;
+        return false;
+    }
+    
+public:
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        m = grid.size(), n = grid[0].size();
+        int visited[41][41][1601];
+        memset(visited, false, sizeof(visited));
+        
+        queue<vector<int>> q1;
+        int steps = 0;
+        
+        q1.push({0, 0, k});
+        visited[0][0][k] = true;
+        
+        while(!q1.empty()){
+            int size = q1.size();
+            
+            while(size--){
+                vector<int> curr = q1.front();
+                q1.pop();
+                
+                int obs = curr[2];
+                
+                if(curr[0] == m-1 && curr[1] == n-1)
+                    return steps;
+                
+                for(auto &dir : dirc){
+                    int newX = curr[0] + dir.first;
+                    int newY = curr[1] + dir.second;
+                    
+                    if(!isValid(newX, newY)) continue;
+                    
+                    if(grid[newX][newY] == 0 && !visited[newX][newY][obs]){
+                        q1.push({newX, newY, obs});
+                        visited[newX][newY][obs] = true;
+                    }else if(grid[newX][newY] == 1 && obs>0 && !visited[newX][newY][obs-1]){
+                        q1.push({newX, newY, obs-1});
+                        visited[newX][newY][obs-1] = true;
+                    }
+                }
+            }
+            
+            steps++;
+        }
+        
+        return -1;
+    }
+};
