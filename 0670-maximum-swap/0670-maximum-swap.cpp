a96cedd32ccc7342f39eb1d827fcda1f633ad493
Rajdev Kapoor <rajdev1409@gmail.com>
2023-10-30 01:58:47 -0400

    Time: 0 ms (100.00%), Space: 6.6 MB (10.01%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maximumSwap(int n) {
+        vector<int> ngr;
+        stack<int> s;
+        vector<int> num;
+        while(n!=0) {
+            num.push_back(n%10);
+            n = n/10;
+        }
+        reverse(num.begin(), num.end());
+
+        vector<int> lgr(num.size(), 0);
+        lgr[num.size()-1] = num.size()-1;
+
+        for(int i=num.size()-2;i>=0;i--) {
+            if(num[i] > num[lgr[i+1]]) {
+                lgr[i] = i;
+            } else {
+                lgr[i] = lgr[i+1];
+            }
+        }
+
+        for(int i=0;i<lgr.size(); i++) {
+            if(num[lgr[i]] != num[i]) {
+                swap(num[lgr[i]], num[i]);
+                break;
+            }
+        }
+
+        int ans = 0;
+
+        for(int i=0; i<num.size(); i++){
+            ans = ans*10 + num[i];
+        }
+
+        return ans;
+        
+    }
+};
