fe1b59a90ed104d849f2ad890a34eb39bc439310
Rajdev Kapoor <rajdev1409@gmail.com>
2023-10-29 22:47:30 -0400

    Time: 123 ms (96.00%), Space: 70.8 MB (96.43%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        if(arr.size()==1){
+            return 0;
+        }
+        if(arr[0] == arr[arr.size()-1]){
+            return 1;
+        }
+        
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]].push_back(i);
+        }
+        
+        int ans = 0;
+        queue<int> q;
+        q.push(0);
+        
+        while(q.size()){
+            ans++;
+            int sz = q.size();
+            
+            for(int i=0;i<sz;i++){
+                
+                auto parent = q.front(); q.pop();
+                
+                if (parent - 1 >= 0 and mp.find(arr[parent - 1]) != mp.end()) {
+                    q.push(parent - 1);
+                }
+                
+                if(parent + 1< arr.size() and mp.find(arr[parent+1]) != mp.end()){
+                    if(parent+1==arr.size()-1){
+                        return ans;
+                    }
+                    q.push(parent + 1);
+                }
+                
+                if (mp.find(arr[parent]) != mp.end()) {
+                    for (auto k : mp[arr[parent]]) {
+                        if (k != parent) {
+                            if (k == arr.size() - 1) return ans;
+                            q.push(k);
+                        }
+                    }
+                }
+                
+                mp.erase(arr[parent]);
+            }
+        }
+        
+        
+        return ans;
+    }
+};
