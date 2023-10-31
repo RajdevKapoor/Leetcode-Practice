40f0c2a3c18ca4f1b565f9619abe62b75de722a6
Rajdev Kapoor <rajdev1409@gmail.com>
2023-10-31 02:10:53 -0400

    Time: 4 ms (43.65%), Space: 8 MB (46.65%) - LeetHub

--- 1024-video-stitching/1024-video-stitching.cpp
+++ 1024-video-stitching/1024-video-stitching.cpp
@@ -1,26 +1,21 @@
-
 class Solution {
 public:
-    int videoStitching(vector<vector<int>>& clips, int T) {
-        
-        int n = clips.size();
-        
-        sort(clips.begin(), clips.end());
-        
-        int max_reach = 0, max_end  = 0, ans  = 0, i = 0;
-        
-        while (max_end < T) {
-            while (i < n && clips[i][0] <= max_end){
-                max_reach = max(max_reach, clips[i][1]);
-                i++;
-            }  
-            
-            if (max_reach <= max_end) return -1;
-            
-            max_end = max_reach;
-            ans++;
+    int videoStitching(vector<vector<int>>& clips, int time) {
+        int count = 0;
+        int mini=0;
+        int maxi=0;
+        while(maxi<time){
+            for(int i=0;i<clips.size();i++){
+                int start=clips[i][0];
+                int end=clips[i][1];
+                if(start<=mini && end>=maxi){
+                    maxi=end;
+                }
+            }
+            if(mini==maxi) return -1;
+            mini=maxi;
+            count++;
         }
-        
-        return ans;
+        return count;
     }
 };
