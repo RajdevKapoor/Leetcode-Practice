f7a114d132acb06e4a5bba3ed846a446c97ccecb
Rajdev Kapoor <rajdev1409@gmail.com>
2023-11-03 03:20:41 -0400

    Time: 16 ms (84.56%), Space: 6.8 MB (31.48%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -7,8 +7,8 @@ public:
         for(int x=0; x<9; x++) if(board[x][j] == val) return false;
         for(int y=0; y<9; y++) if(board[i][y] == val) return false;
         for(int x=0; x<3; x++)
-        for(int y=0; y<3; y++)
-            if(board[row+x][column+y] == val) return false;
+            for(int y=0; y<3; y++)
+                if(board[row+x][column+y] == val) return false;
         return true;
     }
     bool solveSudoku(vector<vector<char>> &board, int i, int j)
