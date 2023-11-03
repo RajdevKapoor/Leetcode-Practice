9a65f7ff6888835e02db04cf7d40369a9bdb611e
Rajdev Kapoor <rajdev1409@gmail.com>
2023-11-03 05:19:37 -0400

    Time: 0 ms (100.00%), Space: 7.4 MB (70.37%) - LeetHub

---
+++ 1023-camelcase-matching/1023-camelcase-matching.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string&s,string&t){
+        int j = 0;
+        for(int i = 0;i < s.length();i++){
+            if(j >= t.length()){
+                if(s[i] >= 'A' && s[i] <= 'Z')
+                    return false;
+            }
+            else{
+                if(s[i] >= 'A' && s[i] <= 'Z')
+                    if(s[i] != t[j]) {
+                        return false;
+                    }
+                    else{
+                        j++;
+                    }
+                if(s[i] == t[j])
+                    j++;
+            }
+        }
+        return j >= t.length();
+    }
+    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+        int n = queries.size();
+        vector<bool>ans(n,false);
+        for(int i = 0;i < n;i++) ans[i] = isValid(queries[i],pattern);
+        return ans;
+    }
+};
